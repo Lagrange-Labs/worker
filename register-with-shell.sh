5b0412f1e32d0394ec07451191860c3a29f10e26
Jonathan Easterman <jonathaneasterman@gmail.com>
2024-05-23 01:05:37 +0100

    refactor script into functions for readability

--- register-with-shell.sh
+++ register-with-shell.sh
@@ -37,76 +37,117 @@ MAINNET_CHAINID=1
 SIG_EXPIRY_SECONDS=300
 export ETH_RPC_URL=https://rpc.holesky.ethpandaops.io
 
-chain_id=$(cast chain-id)
-printf "Chain id: ${chain_id}\n"
+# --- Functions ---
 
-# The signature you create + send is valid for SIG_EXPIRY_SECONDS
-(( expiry_timestamp = $SIG_EXPIRY_SECONDS + $(cast block -f timestamp) ))
-
-# Determine contract addresses based on the chain ID
-if [ $chain_id -eq $HOLESKY_CHAINID ]; then
-    echo "Registering with Lagrange ZK Coprocessor on Holesky Testnet"
-    ZKMR_SERVICE_MANAGER_ADDR=0xf98D5De1014110C65c51b85Ea55f73863215CC10
-    ZKMR_STAKE_REGISTRY_ADDR=0xf724cDC7C40fd6B59590C624E8F0E5E3843b4BE4
-    EIGEN_AVS_DIRECTORY_ADDR=0x055733000064333CaDDbC92763c58BF0192fFeBf
-elif [ $chain_id -eq $MAINNET_CHAINID ]; then
-    echo "Mainnet is not live yet"
-    exit 1
-else
-    echo "Unknown chain ID: $chainId"
-    exit 1
-fi
-
-# --- Script Logic ---
 # The lagrange key should only be used for this AVS.
 # It is an ECDSA secp256k1 keypair; the same as ethereum
-LAGR_KEYPAIR=$(openssl ecparam -name secp256k1 -genkey -noout | openssl ec -text -noout)
-LAGR_KEY=0x$(echo $LAGR_KEYPAIR | grep priv -A 3 | tail -n +2 | tr -d '\n[:space:]:' | sed 's/^00//')
-LAGR_PUB_KEY=$(echo $LAGR_KEYPAIR | grep pub -A 5 | tail -n +2 | tr -d '\n[:space:]:' | sed 's/^04//')
+generate_lagrange_keypair() {
+    local keypair
+    keypair=$(openssl ecparam -name secp256k1 -genkey -noout | openssl ec -text -noout)
+    LAGR_KEY=0x$(echo $keypair | grep priv -A 3 | tail -n +2 | tr -d '\n[:space:]:' | sed 's/^00//')
 
-# 32 bytes of X coordinate of ECDSA secp256k1 public key
-pubkeyX=0x${LAGR_PUB_KEY:0:64}
-# 32 bytes of Y coordinate of ECDSA secp256k1 public key
-pubkeyY=0x${LAGR_PUB_KEY:64:64}
+    local pub_key
+    pub_key=$(echo $keypair | grep pub -A 5 | tail -n +2 | tr -d '\n[:space:]:' | sed 's/^04//')
 
-printf "Public Key: $LAGR_PUB_KEY\n"
-printf "pubkeyX: $pubkeyX\n"
-printf "pubkeyY: $pubkeyY\n"
+    # 32 bytes of X coordinate of ECDSA secp256k1 public key
+    PUBKEY_X=0x${pub_key:0:64}
+    # 32 bytes of Y coordinate of ECDSA secp256k1 public key
+    PUBKEY_Y=0x${pub_key:64:64}
+}
+
+# === MODIFY ME ! ===
+# Implement logic to store the $LAGR_KEY securely.
+store_lagrange_key() {
+    # export LAGR_KEY
+    # echo "LAGR_KEY: $LAGR_KEY"
+    echo "Implement the \`store_lagrange_key\` function before registering"
+    exit 1
+}
+
+# Determine contract addresses based on the chain ID
+set_contract_addresses() {
+    local chain_id = $(cast chain-id)
+
+    if [ -z "$chain_id" ]; then
+        echo "Error: Unable to fetch chain ID."
+        exit 1
+    fi
+    printf "Chain id: ${CHAIN_ID}\n"
+
+    if [ "$chain_id" -eq "$HOLESKY_CHAINID" ]; then
+        echo "Registering with Lagrange ZK Coprocessor on Holesky Testnet"
+        ZKMR_SERVICE_MANAGER_ADDR=0xf98D5De1014110C65c51b85Ea55f73863215CC10
+        ZKMR_STAKE_REGISTRY_ADDR=0xf724cDC7C40fd6B59590C624E8F0E5E3843b4BE4
+        EIGEN_AVS_DIRECTORY_ADDR=0x055733000064333CaDDbC92763c58BF0192fFeBf
+    elif [ "$chain_id" -eq "$MAINNET_CHAINID" ]; then
+        echo "Mainnet is not live yet"
+        exit 1
+    else
+        echo "Unknown chain ID: $chain_id"
+        exit 1
+    fi
+}
+
+# The signature you create + send is valid for SIG_EXPIRY_SECONDS
+calculate_expiry_timestamp() {
+    (( EXPIRY_TIMESTAMP = $SIG_EXPIRY_SECONDS + $(cast block -f timestamp) ))
+}
 
 # A salt for your signature
-salt=0x$(openssl rand -hex 32)
+generate_salt() {
+    SALT=0x$(openssl rand -hex 32)
+}
 
+# Call read-only method on eigenlayer contract to get hash to sign for registration tx
+calculate_registration_hash() {
+    HASH=$(cast call $EIGEN_AVS_DIRECTORY_ADDR \
+        "calculateOperatorAVSRegistrationDigestHash(address,address,bytes32,uint256)" \
+        "$OPERATOR_ADDR" \
+        "$ZKMR_SERVICE_MANAGER_ADDR" \
+        "${SALT}" \
+        "${EXPIRY_TIMESTAMP}")
+    if [ -z "$HASH" ]; then
+        echo "Error: Unable to calculate registration hash."
+        exit 1
+    fi
+    printf "\nRegistration hash:\n${HASH}\n"
+}
+
+# Sign the registration hash with your ETH_KEY
+sign_registration_hash() {
+    SIGNATURE=$(cast wallet sign --private-key $ETH_KEY $hash)
+    printf "\nRegistration signature:\n${SIGNATURE}"
+}
+
+# === MODIFY ME ! ===
+# Register with ZKMR contract using your signature and public key component of your new $LAGR_KEY
+register_operator() {
+    cast send $ZKMR_STAKE_REGISTRY_ADDR \
+        "registerOperator((uint256,uint256),(bytes,bytes32,uint256))" \
+        "(${PUBKEY_X},${PUBKEY_Y})" \
+        "(${SIGNATURE},${SALT},${EXPIRY_TIMESTAMP})" \
+        # === MODIFY ME ! ===
+        --private-key $ETH_KEY # This line can be adjusted for other signers
+        # --aws, --ledger, --trezor, etc.
+
+    printf "\nSuccessfully Registered!\n"
+}
+
+## --- Main Script ---
+generate_lagrange_keypair
+set_contract_addresses
+calculate_expiry_timestamp
+generate_salt
+
+printf "PUBKEY_X: $PUBKEY_X\n"
+printf "PUBKEY_Y: $PUBKEY_Y\n"
 printf "\nOperator Address: $OPERATOR_ADDR\n"
 printf "ZKMRServiceManager contract: $ZKMR_SERVICE_MANAGER_ADDR\n"
 printf "ZKMRStakeRegistry contract: $ZKMR_STAKE_REGISTRY_ADDR\n"
-printf "Salt for signature: $salt\n"
-printf "Signature expires at: $expiry_timestamp"
+printf "Salt for signature: $SALT\n"
+printf "Signature expires at: $EXPIRY_TIMESTAMP\n"
 
-# Call read-only method on eigenlayer contract to get hash to sign for registration tx
-hash=$(cast call $EIGEN_AVS_DIRECTORY_ADDR \
-    "calculateOperatorAVSRegistrationDigestHash(address,address,bytes32,uint256)" \
-    "$OPERATOR_ADDR" \
-    "$ZKMR_SERVICE_MANAGER_ADDR" \
-    "${salt}" \
-    "${expiry_timestamp}")
-
-printf "\nRegistration hash:\n${hash}"
-
-# Sign the output
-signature=$(cast wallet sign --private-key $ETH_KEY $hash)
-printf "\nRegistration signature:\n${signature}"
-
-# Register with ZKMR contract using your signature and public key component of your new $LAGR_KEY
-cast send $ZKMR_STAKE_REGISTRY_ADDR \
-    "registerOperator((uint256,uint256),(bytes,bytes32,uint256))" \
-    "(${pubkeyX},${pubkeyY})" \
-    "(${signature},${salt},${expiry_timestamp})" \
-    # === MODIFY ME ! ===
-    --private-key $ETH_KEY # Specify a signer here as you see fit
-    # --aws, --ledger, --trezor, etc.
-
-printf "\nSuccessfully Registered!"
-
-# === MODIFY ME ! ===
-# Modify the script to store the $LAGR_KEY somewhere safe
-# export LAGR_KEY
+calculate_registration_hash
+sign_registration_hash
+register_operator
+store_lagrange_key
